f49732f2ca537d59314a7154aedc5953dc9cb4c2
李子强 <1312702147@qq.com>
2022-05-05 20:37:39 +0800

    2022.05.05

---
+++ plpl-user/src/test/java/plpl/user/controller/FollowsControllerTest.java
@@ -0,0 +1,18 @@
+package plpl.user.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FollowsControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void list() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void delete() {
+    }
+}
